4c656aa360f347ea3c200ad30fb3c7dfd7969b9e
lidong-sal <lidong-sal@360.cn>
2020-10-26 18:38:18 +0800

    add new project

---
+++ test/components/mycomponent.vue
@@ -0,0 +1,36 @@
+<template>
+	<view>
+		<h1 @tap="tap1">{{title}}</h1>
+		<div>{{content}}</div>
+	</view>
+</template>
+
+<script>
+	export default {
+		props:{
+			title: {
+				type: String,
+				default: ""
+			},
+			content: {
+				type: String,
+				default: ""
+			}
+		},
+		data() {
+			return {
+				
+			};
+		},
+		methods: {
+			tap1 : function(){
+				console.log(1);
+				this.$emit('tap1', this.title)
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ test/pages/index/index.vue
@@ -0,0 +1,60 @@
+<template>
+	<view class="">
+		<mycomponent title="hi" content="content...." @tap1="tap"></mycomponent>
+	</view>
+</template>
+
+<script>
+	import uniNumberBox from "@/components/uni-number-box/uni-number-box.vue"
+	import mycomponent from "@/components/mycomponent.vue"
+	export default {
+		data() {
+			return {
+				title: 'Hello....'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			tap : function(e){
+				console.log(e)
+			}
+		},
+		components: {
+			uniNumberBox,
+			mycomponent
+		}
+	}
+</script>
+<style lang="less">
+@import "../../less.less";
+</style>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
