ca719ac847134040f31e34ee1c3418742ee2c473
System Administrator <root@lidongdeMacBook-Pro.local>
2021-02-03 17:03:56 +0800

    add chapter 48 and 49

---
+++ lufei/vue/45-内容回顾/01-webpack-project/src/components/Home/Home.vue
@@ -0,0 +1,39 @@
+// Content.vue
+<template>
+  <div>
+    <h2>{{ msg }}</h2>
+      <!-- v-html v-for一定要加key v-bind : @ v-model -->
+    </div>
+</template>
+
+<script>
+  export default {
+    // 等于实例化对象
+    data(){
+      return {
+      	  msg:'steven'
+      }
+    },
+    methods:{
+
+    },
+    computed:{
+
+    },
+    created(){
+
+    },
+    mounted(){
+
+    },
+    filters:{
+      '过滤器的名字':function(val,a){
+        // a就是lee，val就是当前的数据
+       }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
